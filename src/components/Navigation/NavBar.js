d9ca4bb0088305888a410ac30fae379a9be3ff3e
Manaf P M <manaf.pm@citiustech.com>
2022-12-07 12:37:10 +0530

    styled navbar

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './NavBar.css';
-import logo from './movie-app-logo.png';
+// import logo from './movie-app-logo.png';
 import {BiCameraMovie} from 'react-icons/bi'
 
 const NavBar = () => {
@@ -10,12 +10,14 @@ const NavBar = () => {
               {/* <img className='logo' src={logo} alt='logo'/> */}
               {/* <i className="ssfa-camera-movie" style={{color:'white',fontSize: 40,cursor:"pointer",marginTop:28, padding:2}}></i> */}
               {/* <i class="fa-sharp fa-solid fa-camera-movie"></i> */}
-              <BiCameraMovie size={53} />
+              <BiCameraMovie size={53} id='logo'/>
           </div>
           <div className='navOptions'>
             <input type='text' className='searchInput'/>
-            <i className="fa-solid fa-magnifying-glass-arrow-right" style={{color:'white',fontSize: 22, cursor:"pointer", marginTop:28, position:'absolute'}}></i>
-            <i className="fa-solid fa-user-astronaut" style={{color:'white',float:'right',fontSize: 40,cursor:"pointer",marginTop:20}}></i>
+            <i className="fa-solid fa-magnifying-glass-arrow-right" id='searchIcon'></i>
+            <i className="fa-solid fa-user-astronaut" id='avatar' ></i>
+            {/* style={{ fontSize: 22, cursor:"pointer", marginTop:20, position:'absolute'}} */}
+            {/* style={{float:'right',fontSize: 40,cursor:"pointer",marginTop:13}} */}
           </div>
       </div>
     )
